bcf4fd9dd7100920e5e9b5bb75d342f30945ae8b
mpl <mathieu.lonjaret@gmail.com>
2025-01-10 18:20:57 +0100

    bump

--- localhello/cmd/hello/main.go
+++ localhello/cmd/hello/main.go
@@ -19,7 +19,7 @@ func init() {
 	flag.StringVar(&flagHost, "host", ":9091", "host:port on which we receive start/stop messages")
 }
 
-const newcode = "back with upgrade.hostname"
+const newcode = "manual bump, not valls"
 
 func main() {
 	flag.Parse()
